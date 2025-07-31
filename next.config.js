bdadca37e41c3069271fd9323653c1817f709a84
syamchelluru <syamasundararaochelluru@gmail.com>
2025-07-31 22:22:49 +0530

    Final update before Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   images: { unoptimized: true },
-// };
-
 // module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Required for static export
+};
+
+module.exports = nextConfig;
