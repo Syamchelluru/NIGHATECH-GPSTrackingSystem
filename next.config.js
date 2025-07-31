0d320f8a86f6c3f8e5236ecc6dfcb57cfdb1bc8b
syamchelluru <syamasundararaochelluru@gmail.com>
2025-07-31 23:05:24 +0530

    Working OTP email flow implemented

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Required for static export
+
 };
 
 module.exports = nextConfig;
